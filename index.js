15baf7c44464a0295070eb90158817c288d87ece
Kayla Smith <kytrick@gmail.com>
2017-12-31 00:08:17 -0800

    added cookieSession

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const keys = require("./config/keys");
 require("./models/User"); // models must come before passport
 require("./services/passport"); // models must come before passport
 
 mongoose.connect(keys.mongoURI);
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    keys: [keys.cookieKey], // key used to encrypt our cookie
+  }),
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 //const authRoutes = require('./routes/authRoutes');
 //authRoutes(app);
