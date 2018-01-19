be2158628595c9651a36af1811e6ca62a4ff86fa
Kayla Smith <kytrick@gmail.com>
2018-01-18 20:37:29 -0800

    give a user 5 credits

--- models/User.js
+++ models/User.js
@@ -1,10 +1,14 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 // const Schema = mongoose.Schema; below is es2015 destructuring syntax instead
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
   googleId: String,
+  credits: {
+    type: Number,
+    default: 0,
+  },
 });
 
 // load a schema into mongoose
-mongoose.model("users", userSchema);
+mongoose.model('users', userSchema);
