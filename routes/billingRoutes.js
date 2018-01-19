d7f8d0ca2989217d98be0573c6024d2f321f5f84
Kayla Smith <kytrick@gmail.com>
2018-01-18 20:25:39 -0800

    changed promise to async/await

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,7 +3,13 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
   // watch for post requests that are made to api/stripe route
-  app.post('api/stripe', (req, res) => {
-    console.log(req.body);
+  app.post('api/stripe', async (req, res) => {
+    const charge = await stripe.charges.create({
+      amount: 500,
+      currency: 'usd',
+      description: '$5 for 5 credits',
+      source: req.body.id,
+    });
+    console.log(charge);
   });
 };
