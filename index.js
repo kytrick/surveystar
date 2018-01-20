b7e5345a3478c541ac35e3416c73680302a52be9
Kayla Smith <kytrick@gmail.com>
2018-01-19 23:52:17 -0800

    typo

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV === 'production') {
   // Express will serve up the index.html profile
   // if it doesn't recognize the route
   // catchall
-  const path = requires('path');
+  const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
