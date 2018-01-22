9f97d4e43ebcfe741117570b83ade2662c959b2c
Kayla Smith <kytrick@gmail.com>
2018-01-21 19:08:03 -0800

    survey setup, key setup

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,9 +1,25 @@
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 
+const Survey = mongoose.model('surveys');
+
 module.exports = app => {
+  // this post request route handler will both create a new survey and send out
+  // a big email
   // check if user is logged in and authenticated
+  // check that the user has enough credits
   app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
-    // check that the user has enough credits
+    const { title, subject, body, recipients } = req.body;
+
+    //create a new instance of the Survey model
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+      _user: req.user.id,
+      dateSent: Date.now(),
+    });
   });
 };
