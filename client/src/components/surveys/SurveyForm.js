d97dd28da6f5e9e6076432a0050b4d2a9c784d31
Kayla Smith <kytrick@gmail.com>
2018-01-24 17:09:01 -0800

    refactored survey forms, added navigation control

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -6,17 +6,11 @@ import { reduxForm, Field } from 'redux-form';
 import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
 import validateEmails from '../../utils/validateEmails';
-
-const FIELDS = [
-  { label: 'Survey Title', name: 'title' },
-  { label: 'Subject Line', name: 'subject' },
-  { label: 'Email Body', name: 'body' },
-  { label: 'Recipient List', name: 'emails' },
-];
+import formFields from './formFields';
 
 class SurveyForm extends Component {
   renderFields() {
-    return _.map(FIELDS, ({ label, name }) => {
+    return _.map(formFields, ({ label, name }) => {
       return (
         <Field
           key={name}
@@ -49,12 +43,12 @@ class SurveyForm extends Component {
 function validate(values) {
   const errors = {};
 
-  _.each(FIELDS, ({ name }) => {
+  _.each(formFields, ({ name }) => {
     if (!values[name]) {
       errors[name] = 'You must provide a value';
     }
   });
-  errors.emails = validateEmails(values.emails || '');
+  errors.emails = validateEmails(values.recipients || '');
   return errors;
 }
 export default reduxForm({
--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,14 +1,37 @@
 // SurveyFormReview shows users their form inputs for review
-
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
+import formFields from './formFields';
+import { withRouter } from 'react-router-dom';
+import * as actions from '../../actions/index.js';
 
-const SurveyFormReview = ({ onCancel }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
+  const reviewFields = _.map(formFields, ({ name, label }) => {
+    return (
+      <div key={name}>
+        <label>{label} </label>
+        <div>{formValues[name]}</div>
+      </div>
+    );
+  });
+
   return (
     <div>
-      <h5> Please confirm you entries </h5>
-      <button className="yellow darken-3 btn-flat" onClick={onCancel}>
+      <h5> Please confirm your entries</h5>
+      {reviewFields}
+      <button
+        className="yellow white-text darken-3 btn-flat"
+        onClick={onCancel}
+      >
         Back
+      </button>
+      <button
+        className="green btn-flat right white-text"
+        onClick={() => submitSurvey(formValues, history)}
+      >
+        Send Survey
+        <i className="material-icons right">email</i>
       </button>
     </div>
   );
@@ -19,4 +42,4 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
