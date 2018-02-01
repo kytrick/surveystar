58a8c33a39bcb27b26a5b454fa30db387e9fd0aa
Kayla Smith <kytrick@gmail.com>
2018-01-31 20:32:30 -0800

    using a different local tunnel

--- sendgrid_webhook.sh
+++ sendgrid_webhook.sh
@@ -1,5 +1,8 @@
 function localtunnel {
-  lt -s hihiohohoihsdflkjsldfkjsildfslidjf --port 5000
+  # lt -s hihiohohoihsdflkjsldfkjsildfslidjf --port 5000
+  #ssh -R 80:localhost:5000 serveo.net
+  ssh -o IdentitiesOnly=yes -o IdentityFile=/dev/null -o ServerAliveInterval=60 -o StrictHostKeyChecking=no -o PreferredAuthentications=keyboard-interactive -R 80:localhost:5000 serveo.net
+
 }
 until localtunnel; do
 echo "localtunnel server crashed"
