e95f7f720e20cc2c2fa9e9e07c1d617a766b7d41
Kayla Smith <kytrick@gmail.com>
2018-02-02 22:34:47 -0800

    debugging

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -24,6 +24,7 @@ module.exports = app => {
 
   app.post('/api/surveys/webhooks', (req, res) => {
     const p = new Path('/api/surveys/:surveyId/:choice');
+    console.log(req.body);
     _.chain(req.body) //iterate over req.body
       .map(({ email, url }) => {
         console.log('i hate this url', url);
