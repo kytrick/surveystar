e7da40031e0c7da37cce06a55e24a16dd804503c
Kayla Smith <kytrick@gmail.com>
2018-02-02 20:11:03 -0800

    putting surveys in redux

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,10 +2,12 @@
 import { combineReducers } from 'redux';
 import { reducer as reduxForm } from 'redux-form';
 import authReducer from './authReducer';
+import surveysReducer from './surveysReducer';
 
 //whatever keys we pass into this object are the
 //keys that exist in our state object
 export default combineReducers({
   auth: authReducer,
   form: reduxForm,
+  surveys: surveysReducer,
 });
